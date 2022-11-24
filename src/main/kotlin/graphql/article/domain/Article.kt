bff4530be1b7bfe792ef25e91e19125e1f4da13c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-11-25 02:22:04 +0900

    feat: Add Article

---
+++ src/main/kotlin/graphql/article/domain/Article.kt
@@ -0,0 +1,42 @@
+package graphql.article.domain
+
+import graphql.article.application.dto.CreateArticleDto
+import graphql.article.application.dto.UpdateArticleDto
+import graphql.common.domain.BaseEntity
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+
+@Entity
+@Table(name = "article_info")
+class Article(
+    @Column(name = "subject", nullable = false)
+    var subject: String,
+
+    @Column(name = "content", nullable = false)
+    var content: String,
+
+    @Column(name = "hits", nullable = false, updatable = true)
+    var hits: Int = 0,
+
+    @Column(name = "recommend", nullable = false, updatable = true)
+    var recommend: Int = 0,
+) : BaseEntity<Long>() {
+    constructor(dto: CreateArticleDto) : this(
+        subject = dto.subject,
+        content = dto.content
+    )
+
+    fun updateArticle(updateArticleDto: UpdateArticleDto) {
+        updateArticleDto.subject?.let { this.subject = it }
+        updateArticleDto.content?.let { this.content = it }
+    }
+
+    fun updateHit() {
+        this.hits++
+    }
+
+    fun updateRecommend() {
+        this.recommend++
+    }
+}
