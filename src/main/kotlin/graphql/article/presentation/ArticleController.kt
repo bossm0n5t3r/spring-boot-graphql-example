b2fd9a53d1d1ee327c2a15d26f6938451d4c8aaa
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-11-29 02:51:10 +0900

    fix: Edit ArticleController
    - Add all mappings of articles

--- src/main/kotlin/graphql/article/presentation/ArticleController.kt
+++ src/main/kotlin/graphql/article/presentation/ArticleController.kt
@@ -1,6 +1,10 @@
 package graphql.article.presentation
 
 import graphql.article.application.ArticleService
+import graphql.article.application.dto.CreateArticleDto
+import graphql.article.application.dto.UpdateArticleDto
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 
@@ -8,6 +12,21 @@ import org.springframework.stereotype.Controller
 class ArticleController(
     private val articleService: ArticleService,
 ) {
-    @QueryMapping(name = "getAllArticles")
-    fun getAllArticles() = articleService.findAll()
+    @QueryMapping
+    fun allArticles() = articleService.findAll()
+
+    @QueryMapping
+    fun article(@Argument articleId: Long) = articleService.findById(articleId)
+
+    @MutationMapping
+    fun createArticle(@Argument dto: CreateArticleDto) = articleService.createArticle(dto)
+
+    @MutationMapping
+    fun updateArticle(
+        @Argument articleId: Long,
+        @Argument dto: UpdateArticleDto,
+    ) = articleService.updateArticle(articleId, dto)
+
+    @MutationMapping
+    fun deleteArticle(@Argument articleId: Long) = articleService.deleteArticle(articleId)
 }
