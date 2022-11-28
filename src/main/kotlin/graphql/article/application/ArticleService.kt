335b67e1e885336ca593ee408e1505f72ed42fd5
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-11-29 02:04:40 +0900

    fix: Edit ArticleService
    - findById
    - createArticle
    - updateArticle
    - deleteArticle

--- src/main/kotlin/graphql/article/application/ArticleService.kt
+++ src/main/kotlin/graphql/article/application/ArticleService.kt
@@ -1,13 +1,48 @@
 package graphql.article.application
 
+import graphql.article.application.dto.CreateArticleDto
+import graphql.article.application.dto.UpdateArticleDto
+import graphql.article.domain.Article
 import graphql.article.domain.ArticleRepository
+import graphql.article.presentation.dto.ArticleDto
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
-@Transactional
 @Service
 class ArticleService(
     private val articleRepository: ArticleRepository,
 ) {
     fun findAll() = articleRepository.findAll().map { it.toDto() }
+
+    fun findById(articleId: Long): ArticleDto? {
+        return articleRepository
+            .findById(articleId)
+            .orElse(null)
+            ?.toDto()
+    }
+
+    @Transactional
+    fun createArticle(dto: CreateArticleDto): ArticleDto {
+        return articleRepository.save(Article(dto)).toDto()
+    }
+
+    @Transactional
+    fun updateArticle(articleId: Long, dto: UpdateArticleDto): ArticleDto? {
+        return articleRepository
+            .findById(articleId)
+            .orElse(null)
+            ?.let {
+                it.updateArticle(dto)
+                it.toDto()
+            }
+    }
+
+    @Transactional
+    fun deleteArticle(articleId: Long): ArticleDto? {
+        return articleRepository
+            .findById(articleId)
+            .map { article -> article.toDto() }
+            .orElse(null)
+            ?.also { articleRepository.deleteById(articleId) }
+    }
 }
